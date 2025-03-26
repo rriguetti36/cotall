b78f7b818dc95209591cbb6d38de15160ea6dbed
roger ramirez <rogier.riguetti@gmail.com>
2025-03-26 02:14:35 -0500

    conexion se cierra pronto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -26,7 +26,7 @@ exports.createProductoForm = (req, res) => {
         return res.status(500).send("Error al obtener marcas");
       }
       console.log(marcas);
-      res.render("productos/create", { categorias, marcas });
+      res.render("Productos/create", { categorias, marcas });
     })
   });
 };
