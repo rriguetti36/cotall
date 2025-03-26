42bb38821597ed9037719c6b8d7e1540145b0083
roger ramirez <rogier.riguetti@gmail.com>
2025-03-26 12:08:10 -0500

    correciones

--- config/db.js
+++ config/db.js
@@ -1,7 +1,26 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error al conectar a la base de datos: ', err);
+//     process.exit(1);
+//   } else {
+//     console.log('Conectado a la base de datos');
+//   }
+// });
+
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -9,15 +28,6 @@ const db = mysql.createConnection({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos: ', err);
-    process.exit(1);
-  } else {
-    console.log('Conectado a la base de datos');
-  }
 });
 
 module.exports = db;
